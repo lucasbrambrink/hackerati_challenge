9e52de511a3c1584b9d3452c9e524201c1350879
lucasbrambrink <lbrambrink@gmail.com>
2015-04-28 18:18:34 -0400

    added main.js script to handle hover in a very basic fashion for now

--- auction/static/js/main.js
+++ auction/static/js/main.js
@@ -2,5 +2,14 @@
  * Created by lb on 4/27/15.
  */
 $(document).ready(function() {
-    
+
+    $('.auction-item').hover(function(){
+        var auctionID = $(this).context.id.split('-')[2];
+        var matchingID = "#auction-" + auctionID;
+
+        $('.auction-container').each(function() {
+            $(this).css('visibility', 'hidden');
+        });
+        $(matchingID).css('visibility', 'visible');
+    });
 });
