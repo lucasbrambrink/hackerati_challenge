f22d7631acc834287471c2e201978e2e3838b633
lucasbrambrink <lbrambrink@gmail.com>
2015-04-30 15:25:01 -0400

    added new pictures and models

--- auction/management/commands/run_import.py
+++ auction/management/commands/run_import.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from auction.scripts import ImportHandler
+from auction.import_scripts import ImportHandler
 
 class Command(BaseCommand):
     help = 'Imports new items from craigslist'
