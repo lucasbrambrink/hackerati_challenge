e9ba87e5fedb3d1703c9a4bbef76dca05eb58d7b
lucasbrambrink <lbrambrink@gmail.com>
2015-04-30 14:21:38 -0400

    included explicit media path

--- auction_hackerati/urls.py
+++ auction_hackerati/urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns('',
     url(r'^on-boarding/', include('base.urls'), name='on_boarding'),
     url(r'^auction/', include('auction.urls'), name='auction'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
